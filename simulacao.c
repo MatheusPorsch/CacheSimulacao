78e4facb7e52284bfd5fb831990fa69c62de25d3
MatheusPorsch <155990907+MatheusPorsch@users.noreply.github.com>
2026-06-14 23:31:37 -0300

    Conversão de endereço de hexadecimal para binário e início da funcionalidade de simulação

--- simulacao.c
+++ simulacao.c
@@ -154,46 +154,149 @@ void mostrar_parametros(int politica_de_escrita, int tamanho_da_linha, int numer
 // Mostrar os Totais de Endereços de Leitura, de Escrita e Gerais
 void mostrar_total_de_enderecos(FILE *arquivo){
 
-    char linha1[100];
-    char linha2[100];
+    char linha1[9], linha2[2];
     int quantidade_de_leituras;
     int quantidade_de_escritas;
 
+    quantidade_de_leituras = 0;
+    quantidade_de_escritas = 0;
+
+    rewind(arquivo);
+
     while(fscanf(arquivo, "%s %s", &linha1, &linha2) != EOF){
+
         if(strcmp(linha2, "R") == 0)
             quantidade_de_leituras++;
-        else 
-            quantidade_de_escritas++;
+        else
+            if(strcmp(linha2, "W") == 0)
+                quantidade_de_escritas++;
     }
 
     printf("Leituras: %d\nEscritas: %d\nTotal %d\n", quantidade_de_leituras, quantidade_de_escritas, quantidade_de_leituras + quantidade_de_escritas);
 }
 
+// Leitura
+void leitura(Cache *cache, char binario[32], int tag_base2, int index_base2){
+
+
+    // strncpy(tag, binario, tag_base2/4);
+
+    // printf("%s\n", endereco);
+
+    // printf("%d, %d\n", sizeof(index), sizeof(tag));
+
+    // printf("%d\n", tag_base2);
+
+    // printf("----------\n");
+
+    // // printf("%d\n", index_base2);;
+
+
+
+    // // printf("%s, %s\n", tag, index);
+    // printf("Tag: %s\n", tag);
+    // // printf("Index: %s\n", index);
+
+}
+
+// Escrita
+void escrita(Cache *cache, char endereco[7]){
+
+}
+
 // Mostrar o Hit Time Médio com Base na Fórmula
 void mostrar_tempo_medio_de_acerto(int hit_time, int tempo_de_leitura_e_escrita, double hit_rate){
 
-    double miss_rate;
-    double tempo_medio_de_acesso = 0.0;
+    double miss_rate, tempo_medio_de_acesso = 0.0;
     
     tempo_medio_de_acesso = hit_time + (1.0 - hit_rate) * tempo_de_leitura_e_escrita;
 
     printf("Tempo medio de acesso: %.4f ns\n");
 }
 
+char * hexa_para_binario(char hexa[9]){
+    
+    char valor_hexa, binario[33];
+
+    binario[0] = '\0';
+    printf("%s\n", binario);
+
+    for(int i = 0; i<8; i++){
+        
+        valor_hexa = hexa[i];
+
+        switch(valor_hexa){
+            case '0':
+                strcat(binario, "0000");
+                break;
+            case '1':
+                strcat(binario, "0001");
+                break;
+            case '2':
+                strcat(binario, "0010");
+                break;
+            case '3':
+                strcat(binario, "0011");
+                break;
+            case '4':
+                strcat(binario, "0100");
+                break;
+            case '5':
+                strcat(binario, "0101");
+                break;
+            case '6':
+                strcat(binario, "0110");
+                break;
+            case '7':
+                strcat(binario, "0111");
+                break;
+            case '8':
+                strcat(binario, "1000");
+                break;
+            case '9':
+                strcat(binario, "1001");
+                break;
+            case 'a':
+                strcat(binario, "1010");
+                break;
+            case 'b':
+                strcat(binario, "1011");
+                break;
+            case 'c':
+                strcat(binario, "1100");
+                break;
+            case 'd':
+                strcat(binario, "1101");
+                break;
+            case 'e':
+                strcat(binario, "1110");
+                break;
+            case 'f':
+                strcat(binario, "1111");
+                break;
+                 
+        }
+    }
+
+    strcat(binario, "\0");
+    printf("%s\n", binario);
+    return binario;
+}
+
+
 // Principal
 int main(){
 
     FILE *arquivo;
-    int politica_de_escrita, tamanho_da_linha, numero_de_linhas, associatividade, hit_time, politica_de_substituicao, tempo_de_leitura_e_escrita;
+    int politica_de_escrita, tamanho_da_linha, numero_de_linhas, associatividade, hit_time, politica_de_substituicao, tempo_de_leitura_e_escrita, hit_rate, numero_de_conjuntos, tag_base2, index_base2, offset_base2;;
 
-    arquivo = fopen("teste.txt", "r");
+
+    arquivo = fopen("C:\\Users\\Matheus\\Documents\\TDE2FArquitetura\\teste.txt", "r");
     if(arquivo == NULL){
         printf("Erro na Abertura do Arquivo \n");
         exit(2);
     }
-
-    int politica_de_escrita, tamanho_da_linha, numero_de_linhas, associatividade, hit_time, politica_de_substituicao, tempo_de_leitura_e_escrita, hit_rate;
-
+    
     // Capturar Parâmetros da Simulação
     politica_de_escrita = get_politica_de_escrita();
     tamanho_da_linha = get_tamanho_da_linha();
@@ -203,13 +306,40 @@ int main(){
     politica_de_substituicao = get_politica_de_substituicao();
     tempo_de_leitura_e_escrita = get_tempo_de_leitura_e_escrita();
 
+
     Cache *cache;
     cache = (Cache *) malloc(sizeof(Cache));
     inicializar_cache(cache);
 
     ///////////////////////
 
-    
+    numero_de_conjuntos = numero_de_linhas/associatividade;
+    index_base2 = log2(numero_de_conjuntos);
+    offset_base2 = log2(tamanho_da_linha);
+    tag_base2 = 32 - index_base2 - offset_base2;
+
+    printf("Index: %d\n", index_base2);
+    printf("Offset: %d\n", offset_base2);
+    printf("Conjuntos: %d\n", numero_de_conjuntos);
+    printf("Tag: %d\n", tag_base2);
+
+    char linha1[10], linha2[10];
+
+    while(fscanf(arquivo, "%s %s", &linha1, &linha2) != EOF){
+
+        if(strcmp(linha2, "R") == 0){
+            leitura(cache, hexa_para_binario(linha1), tag_base2, index_base2);
+        }
+        // else 
+        //     if(strcmp(linha2, "W") == 0)
+        //         escrita(cache, linha1); // Checar se write through ou write back 
+        //     else {
+        //         printf("Erro de endereçamento");
+        //         exit(10);
+        //     }
+
+    }
+
 
     ///////////////////////
 
@@ -219,20 +349,21 @@ int main(){
     // Mostrar os Totais de Endereços de Leitura, de Escrita e Geral do Arquivo
     mostrar_total_de_enderecos(arquivo);
 
-    // Mostrar Total de Escritas na Memória Principal
-    mostrar_total_de_escritas();
+    // // Mostrar Total de Escritas na Memória Principal
+    // mostrar_total_de_escritas();
     
-    // Mostrar Total de Leituras na Memória Principal
-    mostrar_total_de_leituras();
+    // // Mostrar Total de Leituras na Memória Principal
+    // mostrar_total_de_leituras();
 
-    // Mostrar Taxa de Acerto (Hit Rate)
-    mostrar_taxa_de_acerto();
+    // // Mostrar Taxa de Acerto (Hit Rate)
+    // mostrar_taxa_de_acerto();
 
     // Mostrar Tempo Médio de Acerto
     mostrar_tempo_medio_de_acerto(hit_time, tempo_de_leitura_e_escrita, hit_rate);
     
     return 0;
 }
+
 
 
 
